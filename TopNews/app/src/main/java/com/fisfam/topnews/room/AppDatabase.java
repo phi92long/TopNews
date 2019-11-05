9299bf65a3c8c97dff7e9caafb916ac820eb5387
Hoang Hai Ngo <hhaietk@gmail.com>
2019-11-05 23:01:27 +0100

    Simple implementation of persistence cache
    To be developed in future commits

---
+++ TopNews/app/src/main/java/com/fisfam/topnews/room/AppDatabase.java
@@ -0,0 +1,29 @@
+package com.fisfam.topnews.room;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {ArticlesEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase INSTANCE;
+
+    public abstract ArticlesDao articlesDao();
+
+    public static AppDatabase getDb(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "user-database")
+                    //TODO: check if the main thread queries is necessary
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+}
